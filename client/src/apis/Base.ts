062d67155824df1e88786b165c3e9105a04aa4d4
Thanos <dudcjf89@gmail.com>
2022-07-02 18:27:20 +0900

    apis/Base, apis/index ts 로 변경 (PANGPANG-T-9)

--- client/src/apis/Base.ts
+++ client/src/apis/Base.ts
@@ -1,20 +1,18 @@
-import axios from 'axios';
+import axios, {AxiosInstance} from 'axios';
 
 export class Base {
     static setInstance(baseUrl) {
         return (path) => {
-            const instance = axios.create({
+            return axios.create({
                 baseURL: `${baseUrl}${path}`,
                 // withCredentials: true,
             });
-
-            return instance;
         };
     }
 
     baseApi = 'http://localhost:8001';
-    baseApiInstance = Base.setInstance(this.baseApi);
-    base;
+    baseApiInstance: (path) => AxiosInstance = Base.setInstance(this.baseApi);
+    base: AxiosInstance;
 
     constructor() {
         this.base = this.baseApiInstance('/');
