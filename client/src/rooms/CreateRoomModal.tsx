2003b92b68720a52f61534eb3b0edf367abf9657
Thanos <dudcjf89@gmail.com>
2022-07-02 18:06:05 +0900

    CreateRoomModal에 타입 추가 (PANGPANG-T-3)

---
+++ client/src/rooms/CreateRoomModal.tsx
@@ -0,0 +1,33 @@
+import {Button, Form, Input, Modal} from "antd";
+import FormItem from "antd/lib/form/FormItem";
+import {useCallback} from "react";
+
+interface Props {
+    visible: boolean;
+    onCancel: () => void;
+}
+
+export function CreateRoomModal({visible, onCancel}: Props) {
+    const handleOnCancel = () => {
+        onCancel();
+    }
+
+    const handleFinish = useCallback((values) => {
+        console.log("values", values.title)
+    }, []);
+
+    return <Modal width={300} title={'방 만들기'} visible={visible} onCancel={handleOnCancel} footer={[
+        <Button onClick={handleOnCancel}>취소</Button>,
+        <Button htmlType="submit" form="modal-form" loading={false} type={'primary'}>
+            만들기
+        </Button>,
+    ]}>
+        <Form id="modal-form" onFinish={handleFinish}>
+            <FormItem name="title" style={{margin: 0}}>
+                <Input maxLength={16}/>
+            </FormItem>
+        </Form>
+    </Modal>
+}
+
+export default CreateRoomModal
