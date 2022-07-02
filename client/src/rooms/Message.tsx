7daf17a866a3104b68534ea01bb45f3554b1a7da
LimFull <full7002@naver.com>
2022-07-02 18:13:02 +0900

    Message에 타입 추가 (PANGPANG-T-4)

--- client/src/rooms/Message.tsx
+++ client/src/rooms/Message.tsx
@@ -2,9 +2,14 @@ import {Typography} from "antd";
 
 const {Text} = Typography;
 
+interface MessageProps {
+    message:string,
+    name:string,
+}
+
 export function Message({
                             message = '', name = 'default_name'
-                        }) {
+                        }:MessageProps) {
 
     return <Text>{name} : {message}</Text>
 }
